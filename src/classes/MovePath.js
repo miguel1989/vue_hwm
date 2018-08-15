c9323525bf78f8e10dbbfdb5d917e08fe6b95178
mihakras <m.krasikov@gmail.com>
2018-08-15 09:26:13 +0300

    movePath now checks diagonals + tests

--- src/classes/MovePath.js
+++ src/classes/MovePath.js
@@ -6,6 +6,7 @@ export default class MovePath {
     this.cellNumArr = cellNumArr
     this.startPoint = startPoint
     this.endPoint = endPoint
+    // todo take into account isLarge, isFly
   }
 
   // https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%9B%D0%B8
@@ -36,11 +37,29 @@ export default class MovePath {
             if (j + 1 < arrHeight && arr[j + 1][i] === 0) {
               arr[j + 1][i] = nextIndicator
             }
+            // diagonally. left up
+            if (i - 1 >= 0 && j - 1 >= 0 && arr[j - 1][i - 1] === 0) {
+              arr[j - 1][i - 1] = nextIndicator
+            }
+            // diagonally. right up
+            if (i + 1 < arrWidth && j - 1 >= 0 && arr[j - 1][i + 1] === 0) {
+              arr[j - 1][i + 1] = nextIndicator
+            }
+            // diagonally. left down
+            if (i - 1 >= 0 && j + 1 < arrHeight && arr[j + 1][i - 1] === 0) {
+              arr[j + 1][i - 1] = nextIndicator
+            }
+            // diagonally. right down
+            if (i + 1 < arrWidth && j + 1 < arrHeight && arr[j + 1][i + 1] === 0) {
+              arr[j + 1][i + 1] = nextIndicator
+            }
           }
         }
       }
       indicator++
     } while (arr[this.endPoint.y][this.endPoint.x] <= 0 && idx < totalSize)
+
+    console.log(arr)
 
     if (arr[this.endPoint.y][this.endPoint.x] > 0) {
       let pathArr = []
@@ -69,17 +88,37 @@ export default class MovePath {
     }
     pathArr.push(new Point(curX, curY))
     let targetNum = arr[curY][curX] - 1
+    // left
     if (curX - 1 >= 0 && arr[curY][curX - 1] === targetNum) {
       return this._recurFindPath(arr, curX - 1, curY, pathArr)
     }
+    // up
     if (curY - 1 >= 0 && arr[curY - 1][curX] === targetNum) {
       return this._recurFindPath(arr, curX, curY - 1, pathArr)
     }
+    // right
     if (curX + 1 < arr[0].length && arr[curY][curX + 1] === targetNum) {
       return this._recurFindPath(arr, curX + 1, curY, pathArr)
     }
+    // down
     if (curY + 1 < arr.length && arr[curY + 1][curX] === targetNum) {
       return this._recurFindPath(arr, curX, curY + 1, pathArr)
+    }
+    // diagonally. left up
+    if (curX - 1 >= 0 && curY - 1 >= 0 && arr[curY - 1][curX - 1] === targetNum) {
+      return this._recurFindPath(arr, curX - 1, curY - 1, pathArr)
+    }
+    // diagonally. right up
+    if (curX + 1 < arr[0].length && curY - 1 >= 0 && arr[curY - 1][curX + 1] === targetNum) {
+      return this._recurFindPath(arr, curX + 1, curY - 1, pathArr)
+    }
+    // diagonally. left down
+    if (curX - 1 >= 0 && curY + 1 < arr.length && arr[curY + 1][curX - 1] === targetNum) {
+      return this._recurFindPath(arr, curX - 1, curY + 1, pathArr)
+    }
+    // diagonally. right down
+    if (curX + 1 < arr[0].length && curY + 1 < arr.length && arr[curY + 1][curX + 1] === targetNum) {
+      return this._recurFindPath(arr, curX + 1, curY + 1, pathArr)
     }
   }
 }
--- test/unit/specs/MovePath.spec.js
+++ test/unit/specs/MovePath.spec.js
@@ -3,7 +3,7 @@ import Board from '@/classes/Board'
 import Point from '@/classes/Point'
 
 describe('MovePath', () => {
-  it('should find path without obstacles', () => {
+  it('should find path without obstacles from left top corner', () => {
     let board = new Board(6, 6)
     let movePath = new MovePath(board.toNumArr(), new Point(0, 0), new Point(4, 2))
     expect(movePath.calculate()).toEqual(
@@ -11,9 +11,43 @@ describe('MovePath', () => {
         new Point(4, 2),
         new Point(3, 2),
         new Point(2, 2),
-        new Point(1, 2),
-        new Point(0, 2),
-        new Point(0, 1)
+        new Point(1, 1)
+      ]
+    )
+  })
+  it('should find path without obstacles from bottom left corner', () => {
+    let board = new Board(6, 6)
+    let movePath = new MovePath(board.toNumArr(), new Point(0, 5), new Point(4, 2))
+    expect(movePath.calculate()).toEqual(
+      [
+        new Point(4, 2),
+        new Point(3, 2),
+        new Point(2, 3),
+        new Point(1, 4)
+      ]
+    )
+  })
+  it('should find path without obstacles from bottom right corner', () => {
+    let board = new Board(6, 6)
+    let movePath = new MovePath(board.toNumArr(), new Point(5, 5), new Point(1, 3))
+    expect(movePath.calculate()).toEqual(
+      [
+        new Point(1, 3),
+        new Point(2, 3),
+        new Point(3, 3),
+        new Point(4, 4)
+      ]
+    )
+  })
+  it('should find path without obstacles from top right corner', () => {
+    let board = new Board(6, 6)
+    let movePath = new MovePath(board.toNumArr(), new Point(5, 0), new Point(1, 3))
+    expect(movePath.calculate()).toEqual(
+      [
+        new Point(1, 3),
+        new Point(2, 3),
+        new Point(3, 2),
+        new Point(4, 1)
       ]
     )
   })
